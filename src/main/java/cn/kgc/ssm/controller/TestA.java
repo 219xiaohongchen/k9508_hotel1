9f034cbc7209020d3da6195a24cc14b69a3f4772
219xiaohongchen <1075829412@qq.com>
2020-02-04 18:01:58 +0800

    第二次修改TestA.java文件，由组员3进行提交。。

--- src/main/java/cn/kgc/ssm/controller/TestA.java
+++ src/main/java/cn/kgc/ssm/controller/TestA.java
@@ -5,6 +5,10 @@ public class TestA {
     public static void main(String[] args) {
         System.out.println("第一次修改了此TestA类");
     }
+    public void test01(){
+        System.out.println("组员3写的测试test01方法。。");
+
+    }
 
     public void test02(){
         System.out.println("组员2写的测试test02方法。。");
